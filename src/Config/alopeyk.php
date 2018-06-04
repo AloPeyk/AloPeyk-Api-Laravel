71e4f5c692525dad66f2764dcf80e56fc16f6f92
mahdi <mim.borjian@gmail.com>
2018-06-04 18:12:16 +0430

    add sandbox-api attr 2 alopeyk config 2 able users choose sandbox or main api

--- src/Config/alopeyk.php
+++ src/Config/alopeyk.php
@@ -13,4 +13,17 @@ return [
 
     'access-token' => "PUT-YOUR-ACCESS-TOKEN-HERE",
 
+    /*
+    |--------------------------------------------------------------------------
+    | API SANDBOX-API
+    |--------------------------------------------------------------------------
+    |
+    | This value determines that you'r using api in an experimental
+    | environment called sandbox or using it in operational one.
+    | If disabled, requests sends to operational api server.
+    |
+    */
+
+    'sandbox-api' => true,
+
 ];
